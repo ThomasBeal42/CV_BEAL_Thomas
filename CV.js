fd992299dbc0e4d3c6d70323f636ceea21b19a1c
Thomas <thomas.beal25@gmail.com>
2024-10-16 15:51:23 +0200

    commit

--- CV.js
+++ CV.js
@@ -1,1 +1,44 @@
-function scrollToTop(){window.scrollTo({top:0,behavior:"smooth"})}window.onscroll=function(){const t=document.getElementById("scrollUp");100<document.body.scrollTop||100<document.documentElement.scrollTop?t.style.display="flex":t.style.display="none"},document.addEventListener("DOMContentLoaded",function(){const t=document.getElementById("darkModeToggle"),o=document.getElementById("githubButton");t&&(t.addEventListener("click",function(){document.body.classList.toggle("dark-mode"),document.body.classList.contains("dark-mode")?localStorage.setItem("theme","dark"):localStorage.setItem("theme","light")}),"dark"===localStorage.getItem("theme")&&document.body.classList.add("dark-mode")),o&&o.addEventListener("click",function(){window.location.href="https://github.com"})});
+window.onscroll = function() {
+  const scrollUpButton = document.getElementById("scrollUp");
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollUpButton.style.display = "flex";
+  } else {
+    scrollUpButton.style.display = "none";
+  }
+};
+
+// Scroll to Top Function
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const toggleDarkMode = document.getElementById('darkModeToggle');
+  const githubButton = document.getElementById('githubButton');
+
+  // Dark Mode Toggle
+  if (toggleDarkMode) {
+    toggleDarkMode.addEventListener('click', function() {
+      document.body.classList.toggle('dark-mode');
+      
+      // Save user's preference in local storage
+      if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+      } else {
+        localStorage.setItem('theme', 'light');
+      }
+    });
+
+    // Apply the saved theme on page load
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      document.body.classList.add('dark-mode');
+    }
+  }
+
+  githubButton.addEventListener('click', function() {
+    window.open('https://github.com', '_blank', 'noopener noreferrer');
+  });
+  
+  }
+);
